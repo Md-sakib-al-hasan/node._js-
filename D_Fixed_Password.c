e62d9a26e0715c23c4994ddd7f0d45d302f840c6
Md Sakib al hasan <127658885+Md-sakib-al-hasan@users.noreply.github.com>
2023-04-28 15:52:53 +0600

    Add files via upload

---
+++ D_Fixed_Password.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main ()
+{
+    int a;
+    for(int i = -3;0>-3;i--){
+        scanf("%d",&a);
+        if(a==1999){
+            printf("Correct");
+            break;
+        }else{
+            printf("Wrong\n");
+        }
+    }
+    return 0;
+}
